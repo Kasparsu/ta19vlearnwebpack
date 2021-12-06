15eebbb7f8fa6b81d9c0104614e019a588d790bd
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-12-06 14:45:16 +0200

    add chuck norris api example

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
-<div>
-  <!-- <todo-example></todo-example> -->
-    <button class="button is-primary" @click="active=true">Click me</button>
-    <button class="button is-primary" @click="active2=true">Click me too</button>
-    <modal v-if="active" @close="active=false" img="http://placekitten.com/800/600"></modal>
-    <modal v-show="active2" @close="active2=false" img="http://placebear.com/800/600"></modal>
-</div>
+    <div>
+        <h1 class="is-size-1">{{joke}}</h1>
+    </div>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
-import TodoExample from './components/TodoExample.vue'
+const axios = require('axios');
 export default {
-    components: { TodoExample, Modal },
+    created(){
+        axios.get('https://api.chucknorris.io/jokes/random').then(response => {
+            console.log(response);
+            this.joke = response.data.value;
+        });
+    },
     data() {
         return {
-            active: false,
-            active2: false
+          joke: ''
         }
     }
 }
